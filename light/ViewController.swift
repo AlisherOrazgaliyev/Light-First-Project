3fb7a3b3c18393ea615d3c0135881983f76f797e
Alisher <oae.co@yandex.ru>
2019-01-16 12:44:52 +0600

    Initial Commit

---
+++ light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  light
+//
+//  Created by Medina Tugaibayeva on 16.01.2019.
+//  Copyright © 2019 Medina Tugaibayeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
